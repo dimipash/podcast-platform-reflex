e26536f1331454032ad856ba0226e207e3d62376
Dimitar Pashev <dim.pashev@gmail.com>
2025-08-29 17:48:52 +0300

    integrate Reflex with Postgres through Dotenv

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,17 @@
+import os
 import reflex as rx
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+print(DATABASE_URL)
 
 config = rx.Config(
     app_name="podcast_platform_reflex",
     plugins=[
         rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
+        rx.plugins.TailwindV4Plugin(),        
     ],
+    db_url=DATABASE_URL,
 )
