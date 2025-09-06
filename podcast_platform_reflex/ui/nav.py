88347e57fedd5cdfe13687fdd3a560272a8c3073
Dimitar Pashev <dim.pashev@gmail.com>
2025-09-06 17:35:11 +0300

    login, logout, sign up users

--- podcast_platform_reflex/ui/nav.py
+++ podcast_platform_reflex/ui/nav.py
@@ -1,21 +1,25 @@
 import reflex as rx
+import reflex_clerk_api as reclerk
+
 
 def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(
-        rx.text(text, size="4", weight="medium"), href=url
-    )
+    return rx.link(rx.text(text, size="4", weight="medium"), href=url)
 
-def mobile_navbar_link(url:str):
+
+def mobile_navbar_link(url: str):
     return rx.redirect(url)
+
 
 def navbar() -> rx.Component:
     return rx.box(
         rx.desktop_only(
             rx.hstack(
-                rx.hstack(                    
+                rx.hstack(
                     rx.heading(
-                        "Podcast Platform", size="7", weight="bold",
-                        on_click=lambda: mobile_navbar_link("/")
+                        "Podcast Platform",
+                        size="7",
+                        weight="bold",
+                        on_click=lambda: mobile_navbar_link("/"),
                     ),
                     align_items="center",
                 ),
@@ -24,6 +28,20 @@ def navbar() -> rx.Component:
                     navbar_link("About", "/about"),
                     navbar_link("Pricing", "/pricing"),
                     navbar_link("Contact", "/contact"),
+                    rx.fragment(
+                        rx.signed_out(
+                            reclerk.sign_in_button(
+                                rx.button("Sign in", variant="outline")
+                            ),
+                            reclerk.sign_up_button(rx.button("Sign up")),
+                        )
+                    ),
+                    rx.fragment(
+                        rx.signed_in(
+                            reclerk.sign_out_button(rx.button("Sign out")),
+                        )
+                    ),
+                    spacing="3",
                     justify="end",
                     spacing="5",
                 ),
@@ -33,22 +51,28 @@ def navbar() -> rx.Component:
         ),
         rx.mobile_and_tablet(
             rx.hstack(
-                rx.hstack(                    
+                rx.hstack(
                     rx.heading(
-                        "Podcast Platform", size="6", weight="bold", 
-                        on_click=lambda: mobile_navbar_link("/")
+                        "Podcast Platform",
+                        size="6",
+                        weight="bold",
+                        on_click=lambda: mobile_navbar_link("/"),
                     ),
                     align_items="center",
                 ),
                 rx.menu.root(
-                    rx.menu.trigger(
-                        rx.icon("menu", size=30)
-                    ),
+                    rx.menu.trigger(rx.icon("menu", size=30)),
                     rx.menu.content(
                         rx.menu.item("Home", on_click=lambda: mobile_navbar_link("/")),
-                        rx.menu.item("About", on_click=lambda: mobile_navbar_link("/about")),
-                        rx.menu.item("Pricing", on_click=lambda: mobile_navbar_link("/pricing")),
-                        rx.menu.item("Contact", on_click=lambda: mobile_navbar_link("/contact")),
+                        rx.menu.item(
+                            "About", on_click=lambda: mobile_navbar_link("/about")
+                        ),
+                        rx.menu.item(
+                            "Pricing", on_click=lambda: mobile_navbar_link("/pricing")
+                        ),
+                        rx.menu.item(
+                            "Contact", on_click=lambda: mobile_navbar_link("/contact")
+                        ),
                     ),
                     justify="end",
                 ),
